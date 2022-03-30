1f12ff0f616067c7578545430a63b15506582bcb
marcelo quiroz <quiroz_estebanmarcelo@live.com>
2022-03-30 00:45:02 -0300

    Clase 4

---
+++ Clase4/Clase4EjercicioC02/Entidades/Estante.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estante
+    {
+
+        private int ubicacionEstante;
+        private Producto[] productos;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int ubicacion, int capacidad):this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos() 
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstantes(Estante e) 
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Producto item in e.GetProductos())
+            { 
+                sb.AppendLine(Producto.MostrarProducto(item));
+            }
+           
+            return sb.ToString();
+        }
+
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool respuesta = false;
+            foreach (Producto item in e.GetProductos())
+            {
+                if (item == p) 
+                {
+                    respuesta = true;
+                }
+            }
+
+            return respuesta;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+
+            foreach (Producto item in e.GetProductos())
+            {
+               
+            }       
+
+            return !(e == p);
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+
+            return e;
+        }
+
+
+
+
+    }
+}
---
+++ Clase4/Clase4EjercicioC02/Entidades/Producto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+
+
+        public Producto(string codigoDeBarra, string marca, float precio)
+        {
+            this.codigoDeBarra = codigoDeBarra;
+            this.marca = marca;
+            this.precio = precio;
+                
+        }
+
+
+
+        public string GetMarca() 
+        {
+            return this.marca;
+        }
+
+        public static string MostrarProducto(Producto p) 
+        {
+            return $" marca: {p.marca}, precio: {p.precio}, codigo de barra: {(string)p}";
+
+        }
+
+        public static explicit operator string(Producto p) 
+        {
+            return p.codigoDeBarra;
+
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            return (p1.codigoDeBarra == p2.codigoDeBarra && p1.marca==p2.marca);
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+
+        public static bool operator ==(Producto p1, string marcaRecibida)
+        {
+            return (p1.marca == marcaRecibida);
+        }
+
+        public static bool operator !=(Producto p1, string marcaRecibida)
+        {
+            return !(p1 == marcaRecibida);
+        }
+
+
+
+
+    }
+}
